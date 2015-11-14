501a98a329cdf4cc61a2aa3d11422967dcde818e
Morten Kristensen <msk@nullpointer.dk>
2015-11-14 23:19:54 +0100

    Fixed Mac notifications.

--- src/MacNotification.mm
+++ src/MacNotification.mm
@@ -1,16 +1,39 @@
-#import <Foundation/NSUserNotification.h>
+#import <Foundation/Foundation.h>
 
 #include "MacNotification.h"
 
-void showMacMessage(const QString &title, const QString &msg, QSystemTrayIcon::MessageIcon icon,
-                    int timeoutMs) {
-  NSLog(@"sending notification");
+@interface MacNotification : NSObject <NSUserNotificationCenterDelegate>
+@end
 
+@implementation MacNotification
+
+- (BOOL)userNotificationCenter:(NSUserNotificationCenter*)center
+     shouldPresentNotification:(NSUserNotification*)userNotification;
+{
+  return YES;
+}
+
+- (void)showNotificationWithTitle:(NSString*)title
+     message:(NSString*)msg;
+{
   NSUserNotification *notif = [[[NSUserNotification alloc] init] autorelease];
-  notif.title = @"mnet";
-  notif.informativeText = @"osx notification";
+  notif.title = title;
+  notif.informativeText = msg;
 
-  [[NSUserNotificationCenter defaultUserNotificationCenter] deliverNotification:notif];
+  NSUserNotificationCenter *center = [NSUserNotificationCenter defaultUserNotificationCenter];
+  center.delegate = self;
+  [center deliverNotification:notif];
+}
 
-  NSLog(@"sent notification");
+@end
+
+void showMacMessage(const QString &title, const QString &msg,
+                    QSystemTrayIcon::MessageIcon icon, int timeoutMs) {
+  // NOTE: icon and timeoutMs are ignored right now.
+  NSString *title_ =
+    [NSString stringWithCString:title.toUtf8().constData() encoding:NSUTF8StringEncoding];
+  NSString *msg_ =
+    [NSString stringWithCString:msg.toUtf8().constData() encoding:NSUTF8StringEncoding];
+  MacNotification *mn = [[[MacNotification alloc] init] autorelease];
+  [mn showNotificationWithTitle:title_ message:msg_];
 }
